c8511d53ba031c4740bbd3d0130bbb13e820a6db
Ghisso <39460657+Ghisso@users.noreply.github.com>
2018-10-11 00:19:56 +0900

    Add Paddle Script and Top, Bot, Left and Right Colliders

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    [SerializeField] float screenWidthInUnits = 16.0f;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float mousePosX = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        mousePosX = Mathf.Clamp(mousePosX, 1f, screenWidthInUnits - 1);
+        Vector2 paddlePos = new Vector2(mousePosX, transform.position.y);
+        transform.position = paddlePos;
+	}
+}
