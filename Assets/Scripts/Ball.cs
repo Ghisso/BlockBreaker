c3e565b2ca88eb1559cedaf12eb3874e40b24aeb
Ghisso <ghisso@gmail.com>
2018-10-11 14:56:36 +0900

    Ball and Block scripts
    The ball sticks to paddle until mouse click and collides against surfaces.
    Block has "Breakable" tag and gets destroyed on Collision.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,14 +4,40 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    bool isStarted = false;
+    Paddle paddle;
+    float yOffset, xPush = 2.0f, yPush = 15.0f, randomPush = 0.1f;
+    Rigidbody2D myRigidbody2D;
+
+    // Use this for initialization
+    void Start ()
+    {
+        paddle = FindObjectOfType<Paddle>();
+        yOffset = transform.position.y - paddle.transform.position.y;
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        if(!isStarted)
+        {
+            Vector2 newBallPos = new Vector2(paddle.transform.position.x, paddle.transform.position.y + yOffset);
+            transform.position = newBallPos;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            isStarted = true;
+            myRigidbody2D.velocity = new Vector2(xPush, yPush);
+        }
+            
 	}
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector2 velocityTweak = new Vector2(randomPush, randomPush);
+        myRigidbody2D.velocity += velocityTweak;
+    }
 
 }
---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour 
+{
+    BoxCollider2D myBoxCollider2D;
+    SpriteRenderer mySpriteRenderer;
+
+	// Use this for initialization
+	void Start ()
+    {
+        myBoxCollider2D = GetComponent<BoxCollider2D>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (tag == "Breakable")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
